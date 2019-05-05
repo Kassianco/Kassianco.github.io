4a9929087b486c9ac84adde4e1e91570efa00396
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-05-05 19:22:30 +0200

    update with js

--- Script/animation_desktop.js
+++ Script/animation_desktop.js
@@ -1,4 +1,17 @@
 console.log('index');
 
 var myAnimation = new TimelineMax();
-myAnimation.to('.j-project1', 1, { y: -100 });
+myAnimation.to('.my_name', 1, { x:100});
+
+
+
+var myAnimation = new TimelineMax();
+myAnimation.to('', 1, { y: -100});
+
+var controller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+  triggerElement: '#trigger',
+  triggerHook: .6,
+  offset: 0,
+  duration: 750
+}).setTween(myAnimation).addTo(controller); 
