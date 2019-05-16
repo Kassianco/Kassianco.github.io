cf29f29b106f2bb9aaf80a545c72d81c70f93645
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-05-16 20:17:18 +0200

    update

--- Script/fendi_page_animation.js
+++ Script/fendi_page_animation.js
@@ -7,8 +7,7 @@ myAnimation.to('.j-project_img_list', .1, {opacity: 1});
 var controller = new ScrollMagic.Controller();
 var scene = new ScrollMagic.Scene({
   triggerElement: '#j-intro_trigger',
-  triggerHook: .5,
+  triggerHook: .1,
   offset: 0,
   duration: 1000,
-  opacity:1
 }).setTween(myAnimation).addTo(controller); 
