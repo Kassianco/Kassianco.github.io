f2fc8aa58a8a2c56b4a4e64e4da59ac6b70e7fbf
Cordiner Matteo <cordiner@BallmerMac-01.local>
2019-05-16 13:49:14 +0200

    updates

--- Script/fendi_page_animation.js
+++ Script/fendi_page_animation.js
@@ -1,8 +1,14 @@
 console.log('fendi_page')
 
 //Fendi Project Page
-TweenMax.to('j-projectpg_fendi_img', 1, {
-  x: 1, 
-  opacity: 1,
-  ease: Power4.easeOut,
-});
+var myAnimation = new TimelineMax();
+myAnimation.to('.j-project_img_list', .1, {opacity: 1});
+
+var controller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+  triggerElement: '#j-intro_trigger',
+  triggerHook: .5,
+  offset: 0,
+  duration: 1000,
+  opacity:1
+}).setTween(myAnimation).addTo(controller); 
